d488a22fb4fe5eabe7069580ebcdd1463c71d6c1
Angeladrian059 <angeladriand059@gmail.com>
2026-07-09 07:34:53 -0600

    ya termine el caso number 1 =), nomas que tengo errores

--- src/Main.java
+++ src/Main.java
@@ -10,20 +10,53 @@ public static void main(String[] args) {{
         System.out.println("2. Mostrar reporte");
         System.out.println("3. Salir");
         System.out.print("Seleccione una opción: ");
-        try{
+        try {
             opcion = Leer.nextInt();
-        } catch (Exception e){
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Ingrese su Correo Electronico : ");
+                    String correo = Leer.nextLine();
+                    System.out.println("Ingrese la cantidad de meses : ");
+                    int mesesActivo = Leer.nextInt();
+                    Leer.nextLine();
+                    PlanSuscripcion plan = null;
+
+                    switch (opcion) {
+                        case 1:
+                            plan = new PlanBasico();
+                            break;
+
+                        case 2:
+                            plan = new PlanEstandar();
+                            break;
+
+                        case 3:
+                            plan = new PlanPremium();
+                            break;
+
+                        default:
+                            System.out.println("Plan inválido.");
+                            break;
+                    }
+
+                    if (plan != null) {
+                        UsuarioStreaming usuario =
+                                new UsuarioStreaming(correo, mesesActivo, plan);
+
+                        plataforma.registrarUsuarios(usuario);
+
+                        System.out.println("Usuario registrado correctamente.");
+                    }
+
+                    break;
+
+            }
+        }catch (Exception e){
             System.out.println("Ingrese una opcion valida");
             Leer.nextLine();
+
         }
-        switch (opcion){
-            case 1:
-                System.out.println("Ingrese su Correo Electronico : ");
-                String correo = Leer.nextLine();
-                System.out.println("Ingrese la cantidad de meses : ");
-                int mesesActivo = Leer.nextInt();
-                Leer.nextLine();
-                System.out.println("Aqui termine de hacer un menu uwu");
-        }
+
     } while (opcion != 3);
 }
